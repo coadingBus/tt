0a24cad55277855f006087fbee44ee985e81247e
coadingBus <1138848222@qq.com>
2020-05-08 10:16:06 +0800

    TTEST

---
+++ src/Demo2.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        List<String> languages =  Arrays.asList("java","scala","python");
+        //languages.forEach(x-> System.out.println(x));
+        languages.forEach(System.out::println);
+    }
+}
