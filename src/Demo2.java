e49ab3751119e25988057e4c9a3d1fcb4b709dbc
coadingBus <1138848222@qq.com>
2020-05-08 10:22:30 +0800

    ss55

--- src/Demo2.java
+++ src/Demo2.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Demo2 {
     public static void main(String[] args) {
         List<String> languages =  Arrays.asList("java","scala","python");
-        //languages.forEach(x-> System.out.println(x));
+        languages.forEach(x-> System.out.println(x));
         languages.forEach(System.out::println);
     }
 }
